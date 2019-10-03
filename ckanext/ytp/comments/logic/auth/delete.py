0dd9069ed564076b850366bb37136436be9079da
Nicholas Giambruno <ngiambruno@basispath.com>
2019-10-03 16:22:43 -0400

    Removing ckan sysadmin check doe to version incompatability with 2.8.

--- ckanext/ytp/comments/logic/auth/delete.py
+++ ckanext/ytp/comments/logic/auth/delete.py
@@ -1,23 +1,15 @@
 import logging
 from pylons.i18n import _
 
-import ckan.new_authz as new_authz
 from ckan import logic
 import ckanext.ytp.comments.model as comment_model
 
-
 log = logging.getLogger(__name__)
-
 
 def comment_delete(context, data_dict):
     model = context['model']
     user = context['user']
-
     userobj = model.User.get(user)
-    # If sysadmin.
-    if new_authz.is_sysadmin(user):
-        return {'success': True}
-
     cid = logic.get_or_bust(data_dict, 'id')
 
     comment = comment_model.Comment.get(cid)
